4bacbd983684f8f709059529c1f18bec695cfc3c
matthewbergin878 <Matthew.Bergin@cencora.com>
2025-07-17 14:58:26 -0400

    add swagger to try to get api to fire

--- insecure/insecure/urls.py
+++ insecure/insecure/urls.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 from django.urls import path, include
 from django.views.generic.base import TemplateView 
+from insecure.swagger import schema_view
 
 urlpatterns = [
     path("admin/", admin.site.urls),
@@ -8,4 +9,5 @@ urlpatterns = [
     path("accounts/", include("django.contrib.auth.urls")),
     path("users/", include("users.urls")),
     path("", TemplateView.as_view(template_name="home.html"), name="home"),
+    path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
 ]
